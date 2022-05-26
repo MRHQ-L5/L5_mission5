244419b649314e62fb22785331c6926334501c2e
Phillip W <phillipwu9@gmail.com>
2022-05-26 19:40:18 +1200

    made home page blue btns functional

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./ContentSection.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ContentSection() {
+  let navigate = useNavigate();
   return (
     <section className="content_container">
       <div className="text_container">
@@ -20,37 +22,55 @@ export default function ContentSection() {
       <div>
         <div className="content_box">
           <div className="box_outer_btn">
-            <button className="box_btn">Buy a car</button>
+            <button
+              className="box_btn"
+              onClick={() => navigate("/UnderConstruction")}
+            >
+              Buy a car
+            </button>
           </div>
           <div className="inside_box">
             <div className="inside_line">
               <h3 className="h3tag_in_boxes">Buy Now</h3>
-              <p className="ptag_in_boxes">Purchase directly from our <br/>site in branch, on the spot! </p>
+              <p className="ptag_in_boxes">
+                Purchase directly from our <br />
+                site in branch, on the spot!{" "}
+              </p>
             </div>
           </div>
           <div className="inside_box">
             <div className="inside_line">
               <h3 className="h3tag_in_boxes">Online auctions</h3>
-              <p className="ptag_in_boxes"> These are either held exclusively on our <br/>website or in partnership
-                  with Trade me
+              <p className="ptag_in_boxes">
+                {" "}
+                These are either held exclusively on our <br />
+                website or in partnership with Trade me
               </p>
             </div>
           </div>
           <div className="outer_box">
             <h3 className="h3tag_in_boxes">Live auction</h3>
-            <p className="ptag_in_boxes">You can you this online or <br/>come into the branch</p>
+            <p className="ptag_in_boxes">
+              You can you this online or <br />
+              come into the branch
+            </p>
           </div>
         </div>
         <div className="content_box">
           <div className="box_outer_btn">
-            <button className="box_btn">Sell a car</button>
+            <button
+              className="box_btn"
+              onClick={() => navigate("/UnderConstruction")}
+            >
+              Sell a car
+            </button>
           </div>
           <div className="inside_box">
             <div className="inside_line">
               <h3 className="h3tag_in_boxes">Turners CashNow</h3>
               <p className="ptag_in_boxes">
-                Get and offer on the spot, and you’ll have <br/>money in your bank
-                account within 48 hours.
+                Get and offer on the spot, and you’ll have <br />
+                money in your bank account within 48 hours.
               </p>
             </div>
           </div>
@@ -58,38 +78,49 @@ export default function ContentSection() {
             <div className="inside_line">
               <h3 className="h3tag_in_boxes">Trade-in</h3>
               <p className="ptag_in_boxes">
-                Use your old car to help buy your next car. <br/>We can even finance
-                the difference.
+                Use your old car to help buy your next car. <br />
+                We can even finance the difference.
               </p>
             </div>
           </div>
           <div className="outer_box">
             <h3 className="h3tag_in_boxes">Sell by Auction</h3>
             <p className="ptag_in_boxes">
-              Hassel-free. <br/>We will do the hard work for you. <br/>Let the markey
-              decide what your car is worth.
+              Hassel-free. <br />
+              We will do the hard work for you. <br />
+              Let the markey decide what your car is worth.
             </p>
           </div>
         </div>
         <div className="content_box">
           <div className="box_outer_btn">
-            <button className="box_btn">Get car insurance</button>
+            <button className="box_btn" onClick={() => navigate("/Insurance")}>
+              Get car insurance
+            </button>
           </div>
           <div className="inside_box">
             <div className="inside_line">
               <h3 className="h3tag_in_boxes">Get a free quote</h3>
-              <p className="ptag_in_boxes">Get a free quote in 2 minutes. <br/>It's on us!</p>
+              <p className="ptag_in_boxes">
+                Get a free quote in 2 minutes. <br />
+                It's on us!
+              </p>
             </div>
           </div>
           <div className="inside_box">
             <div className="inside_line">
               <h3 className="h3tag_in_boxes">Do it all online</h3>
-              <p className="ptag_in_boxes">You can buy insurance all online, at your pace.</p>
+              <p className="ptag_in_boxes">
+                You can buy insurance all online, at your pace.
+              </p>
             </div>
           </div>
           <div className="outer_box">
             <h3 className="h3tag_in_boxes">Bought your car with us?</h3>
-            <p className="ptag_in_boxes">Get an insurance discount <br/>if you bought your car with us.</p>
+            <p className="ptag_in_boxes">
+              Get an insurance discount <br />
+              if you bought your car with us.
+            </p>
           </div>
         </div>
       </div>
